d963da8bfae378761f0de87d5b2152e716db2357
handsomeskuld <1968567605@qq.com>
2026-05-04 12:17:14 +0800

    基础配置ok

--- src/config/adConfig.ts
+++ src/config/adConfig.ts
@@ -45,7 +45,7 @@ export const adConfig2: AdConfig = {
 	content:
 		"如果您觉得本站内容对您有帮助，欢迎支持我们的创作！您的支持是我们持续更新的动力。",
 	image: {
-		src: "assets/images/zhichi.png",
+		src: "/assets/images/zhichi.png",
 		alt: "支持博主",
 		link: "https://prothistle.com/sponsor/",
 		external: true,
